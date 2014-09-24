9596819e128dd2b752c44d7ba1d90da2eddfb584
biwin <biwin007@gmail.com>
2014-09-24 19:28:52 +0530

    template inheritance.

--- views.py
+++ views.py
@@ -42,6 +42,5 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 
-	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+	date_time = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	return render_to_response('hours_ahead.html', locals())
