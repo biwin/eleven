941fb25b0c0dfe65cd6f744e341ff99711d6eea2
biwin <biwin007@gmail.com>
2014-09-26 15:48:34 +0530

    searchform . .@

--- books/views.py
+++ books/views.py
@@ -1,17 +1,17 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from mysite.books.models import Book
 
 
 def search_form(request):
-	return render_to_response('searchform.html')
+	return render_to_response('search_form.html')
 
 
 def search(request):
-	if 'q' in request.GET:
-		message = 'You searched for: %r' % request.GET['q']
+	if 'q' in request.GET and request.GET['q']:
+		q = request.GET['q']
+		books = Book.objects.filter(title_icontains=q)
+		return render_to_response('search_results.html', {'books': books, 'query': q})
 	else:
-		message = 'You submitted an empty form!!'
-	return HttpResponse(message)
-
-# 123
+		return render_to_response('search_form.html', {'error': True})
