5ddf425e43c326f914fbfb563b1cdaee80056fd0
biwin <biwin007@gmail.com>
2014-09-26 21:44:31 +0530

    refactor the templates.

--- books/views.py
+++ books/views.py
@@ -5,20 +5,20 @@ from mysite.books.models import Book
 
 
 def search_form(request):
-	return render_to_response('search_form.html')
+	return render_to_response('search/search_form.html')
 
 
 def search(request):
-	error = False
+	errors = []
 	if 'q' in request.GET:
 		q = request.GET['q']
 		if not q:
-			error = True
+			errors.append('Enter a search term')
 
 		elif len(q) > 20:
-			error = True
+			errors.append('Enter less than 20 characters including spaces.')
 
 		else:
 			books = Book.objects.filter(title__icontains=q)
-			return render_to_response('search_results.html', {'books': books, 'query': q})
-	return render_to_response('search_form.html', {'error': error})
+			return render_to_response('search/search_results.html', {'books': books, 'query': q})
+	return render_to_response('search/search_form.html', {'errors': errors})
